fdaa50551f9f4007a201fc62a49179979e01fd03
Peter Ericsson <peter.ericsson@addlevel.se>
2019-02-04 12:56:34 +0100

    .

--- file.ps1
+++ file.ps1
@@ -1,15 +1,1 @@
-$globalConfiguration = @{
-    subscriptionId = '123-456-789'
-    Partner = 'Addleve AB'
-    tenantDomain = 'mynenant'
-    resourceCharacterLimit = 19
-    storage = @{
-        type = "Standard_LRS"
-        containers = @("attachments", "documents", "exflowdiagnostics")
-    }
-}
-
-$globalConfigurationJson = ConvertTo-Json $globalConfiguration -Compress
-
-
-{"subscriptionId":"123-456-789","storage":{"containers":["attachments","documents","exflowdiagnostics"],"type":"Standard_LRS"},"resourceCharacterLimit":19,"tenantDomain":"mynenant","Partner":"Addleve AB"}
+\"[concat('{{\"applicationInsightsInstrumentationKey\":\"[AppInsightsKey]\",\"APPINSIGHTS_INSTRUMENTATIONKEY\":\"[AppInsightsKey]\",\"APPINSIGHTS_PORTALINFO\":\"[APPPORTALINFO]\",\"APPINSIGHTS_PROFILERFEATURE_VERSION\":\"[APPPROFILERVERSION]\",\"startup.DeploymentName\":\"[DeploymentName]\",\"Startup.HostRightPart\":\".[TenantName]\",\"Startup.AzureKeyVaultURL\":\"[CustomerKeyVault]\",\"offline_\":\"85.24.197.82\"}}')]\"
