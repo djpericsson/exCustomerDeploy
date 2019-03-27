17959a653504e5eb88a49574c03061d936bf51e8
Peter Ericsson <peter.ericsson@addlevel.se>
2019-03-27 16:14:55 +0100

    .

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,5 @@
+Import-Module Az.Resources
+
 $params = @{
     namespaceName            = 'exky7qibpc7ehfy'
     hybridConnectionName     = 'exky7qibpc7ehfy'
@@ -6,4 +8,4 @@ $params = @{
     namespaceAuthRuleName    = 'defaultSender'
 }
 
-Test-AzureRmResourceGroupDeployment -TemplateFile "C:\Repo\exCustomerDeploy\relayDeployment.json" @params -ResourceGroupName 'SignUp-Test' -location 'westeurope' -Verbose
+Test-azResourceGroupDeployment -TemplateFile "C:\Repo\exCustomerDeploy\relayDeployment.json" @params -ResourceGroupName 'SignUp-Test' -location 'westeurope' -Verbose
